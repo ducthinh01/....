f296b229e34d9f51eae764ae1ee7875024a43608
ducthinh01 <81355156+ducthinh01@users.noreply.github.com>
2021-06-04 12:47:15 +0700

    Add files via upload

---
+++ Tang-em/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Kim Ánh à!',
+    introDesc: `Vì dịch mà chúng ta chưa được gặp nhau hiccc 😔`,
+    btnIntro: 'Bấm vào đây nè',
+    title: 'Hello!!!',
+    desc: 'Nhớ Đức Thịnh hemmmmmm ',
+    btnYes: 'Nhớ lắm luôn ❤️❤️❤️',
+    btnNo: 'Hong nhớ nhaaa 🤪🤪🤪',
+    question:'Có gì nhắn nhủ thì nói dưới đây nè:',
+    btnReply: 'Gửi cho ĐT',
+    reply: 'Nhớ ĐT nhiều lém ❤️❤️❤️',
+    mess: 'Bất ngờ chưaaa 😘😘',
+    messDesc: 'Anh cũng nhớ em 😢😢😢',
+    btnAccept: 'Bấm vào đây để đỡ nhớ nhao nè...',
+    messLink: 'https://m.me/tranducthinh01'
+}
